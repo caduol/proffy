c8c42352ae8b04b70f685e530d983f2e510c2f1f
Cadu <eduardo.w3c@gmail.com>
2020-08-06 02:25:54 -0300

    02 - progress

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+// Corpo ( Request Body ): Dados para criação ou atualização de um registro
+// Route Params: identifica qual recurso dentro de uma rota quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+
+app.listen(3333)
