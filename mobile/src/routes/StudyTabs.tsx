6af1ae3f4cc1277bc46ada92e5478e6e41638247
Cadu <eduardo.w3c@gmail.com>
2020-08-08 23:10:49 -0300

    05 - progress

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -42,7 +42,7 @@ function StudyTabs() {
                     tabBarLabel: 'Proffys',
                     tabBarIcon: ({ color, size, focused }) => {
                         return (
-                            <Ionicons name="ios-easel" size={size} color={focused ? '8257e5' : color} />
+                            <Ionicons name="ios-easel" size={size} color={focused ? '#8257e5' : color} />
                         )
                     }
                 }}
@@ -54,7 +54,7 @@ function StudyTabs() {
                     tabBarLabel: 'Favoritos',
                     tabBarIcon: ({ color, size, focused }) => {
                         return (
-                            <Ionicons name="ios-heart" size={size} color={focused ? '8257e5' : color} />
+                            <Ionicons name="ios-heart" size={size} color={focused ? '#8257e5' : color} />
                         )
                     }
                 }}
